3d05ece4cbebde841a9a8277b8f25012017b2c27
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-16 14:39:00 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,83 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    dart = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 9 9 9 . . . . . . 
+        . . . . . . . 9 6 6 6 . . . . . 
+        9 9 9 9 9 9 9 9 6 6 . . . . . . 
+        6 6 6 6 6 6 6 6 6 6 . . . . . . 
+        8 8 8 8 8 8 8 8 8 8 . . . . . . 
+        . . . . . . . . 8 8 8 . . . . . 
+        . . . . . . . . . . 8 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Spaceplane, 200, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    evilduck.destroy()
+    evilduck.destroy(effects.fire, 100)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    evilduck.destroy()
+    info.changeLifeBy(-1)
+})
+let evilduck: Sprite = null
+let dart: Sprite = null
+let Spaceplane: Sprite = null
+Spaceplane = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Spaceplane.setFlag(SpriteFlag.StayInScreen, true)
+info.setLife(3)
+controller.moveSprite(Spaceplane, 100, 100)
+game.onUpdateInterval(500, function () {
+    evilduck = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f1111111df.......
+        ......fd1111111ddf......
+        ......fd111111dddf......
+        ......fd111ddddddf......
+        ......fd1dfbddddbf......
+        ......fbddfcdbbbcf......
+        .......f11111bbcf.......
+        .......f1b1fffff........
+        .......fbfc111bf........
+        ........ff1b1bff........
+        .........fbfbfff.f......
+        ..........ffffffff......
+        ............fffff.......
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Player)
+    evilduck.setVelocity(-100, 0)
+    evilduck.setPosition(180, randint(0, 120))
+})
